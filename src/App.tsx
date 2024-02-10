702ef8dda4bc284879980a1bb990424513d12787
Saroj Ghalan <jmestmnag@gmail.com>
2024-02-10 09:47:26 +0545

    FlyOut wrapper - context created

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useState,
+} from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+type FlyOutContextType = {
+  open: boolean;
+  toggle: React.Dispatch<SetStateAction<boolean>>;
+};
+
+type AppProps = {
+  children: ReactNode;
+};
+
+const FlyOutContext = createContext({
+  open: false,
+  toggle: () => {},
+} as FlyOutContextType);
+
+function App({ children }: AppProps) {
+  const [open, toggle] = useState<boolean>(false);
+  const contextValue: FlyOutContextType = {
+    open,
+    toggle,
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <FlyOutContext.Provider value={contextValue}>
+      {children}
+    </FlyOutContext.Provider>
+  );
 }
 
-export default App
+export default App;
