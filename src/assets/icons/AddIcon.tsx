f5ea0b35d7a307c9474c6a14181f5c94ff584473
Saroj Ghalan <jmestmnag@gmail.com>
2024-02-13 07:13:24 +0545

    icons

---
+++ src/assets/icons/AddIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+export const AddIcon = (props: React.ComponentProps<"svg">) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path
+        d="M8.00032 2.6665V13.3332M13.3337 7.99984L2.66699 7.99984"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
